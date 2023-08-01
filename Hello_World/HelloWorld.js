2b8fab5bb88639be28f49a8f8939965b12c243f3
Maitrayee Das <maitrayeedas94@gmail.com>
2023-08-01 10:23:32 +0530

    first commit

---
+++ Hello_World/HelloWorld.js
@@ -0,0 +1,48 @@
+import * as LitJsSdk from "@lit-protocol/lit-node-client";
+
+// this code will be run on the node
+const litActionCode = `
+const go = async () => {
+  // this requests a signature share from the Lit Node
+  // the signature share will be automatically returned in the response from the node
+  // and combined into a full signature by the LitJsSdk for you to use on the client
+  // all the params (toSign, publicKey, sigName) are passed in from the LitJsSdk.executeJs() function
+  const sigShare = await LitActions.signEcdsa({ toSign, publicKey, sigName });
+};
+
+go();
+`;
+
+// you need an AuthSig to auth with the nodes
+// normally you would obtain an AuthSig by calling LitJsSdk.checkAndSignAuthMessage({chain})
+const authSig = {
+  sig: "0x2bdede6164f56a601fc17a8a78327d28b54e87cf3fa20373fca1d73b804566736d76efe2dd79a4627870a50e66e1a9050ca333b6f98d9415d8bca424980611ca1c",
+  derivedVia: "web3.eth.personal.sign",
+  signedMessage:
+    "localhost wants you to sign in with your Ethereum account:\n0x9D1a5EC58232A894eBFcB5e466E3075b23101B89\n\nThis is a key for Partiful\n\nURI: https://localhost/login\nVersion: 1\nChain ID: 1\nNonce: 1LF00rraLO4f7ZSIt\nIssued At: 2022-06-03T05:59:09.959Z",
+  address: "0x9D1a5EC58232A894eBFcB5e466E3075b23101B89",
+};
+
+const runLitAction = async () => {
+  const litNodeClient = new LitJsSdk.LitNodeClient({
+    alertWhenUnauthorized: false,
+    litNetwork: "serrano",
+    debug: true,
+  });
+  await litNodeClient.connect();
+  const results = await litNodeClient.executeJs({
+    code: litActionCode,
+    authSig,
+    // all jsParams can be used anywhere in your litActionCode
+    jsParams: {
+      // this is the string "Hello World" for testing
+      toSign: [72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100],
+      publicKey:
+        "0x04fe9f3d0be0241e5b6ec7b654e6e3710ac1fc6bb05e55cb56b8d9216240cb3a4a963034229eb7abbc165f7900b56c05dbbac358bdc45ce7c0388f2429295b912e",
+      sigName: "sig1",
+    },
+  });
+  console.log("results: ", results);
+};
+
+runLitAction();
